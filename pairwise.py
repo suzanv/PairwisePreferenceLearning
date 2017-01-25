f03f6a106491b5e4fb8fd75692661bd3f0a14740
Suzan Verberne <suzanverberne@c001524.srw.ru.nl>
2017-01-25 16:30:41 +0100

    improved readme

--- pairwise.py
+++ pairwise.py
@@ -2,19 +2,17 @@
 
 """
 performs pairwise preference ranking for a given trainfile and testfile with binary class labals (1 and not 1)
-pairwise preference ranking is commonly performed on grouped data: two items from the same group are comparable while
-two items from different groups are not. A typical example is rank-learning in the context of an information retrieval
-system. Here the groups are the queries. Two documents that are retrieved for the same query have a pairwise preference;
-two documents that are retrieved for two different queries do not have a pairwise preference and should not be paired.
+
+Pairwise preference ranking is commonly performed on grouped data: two items from the same group are comparable to each other (in an ordinal version: one is better than the other), while two items from different groups are not. A typical example is rank-learning in the context of an information retrieval system. Here the groups are the queries. Two documents that are retrieved for the same query have a pairwise preference; two documents that are retrieved for two different queries do not have a pairwise preference and should not be compared to each other.
 
 The script requires 2 tab-separated files in which:
 - the first column is the group id (in IR context: query id)
 - the last column is the label (1 = positive, not 1 = negative)
-all the columns in between are numeric feature values
+- all the columns in between are numeric feature values
 
 the train-test split should be on the group level: all items belonging to one group should be in the same partition.
 
-Two example files are provided
+Two example files are provided.
 
 """
 
