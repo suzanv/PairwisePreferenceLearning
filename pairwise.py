03ea44700d8f90f36ae5470bf1e2071d9777da46
Suzan Verberne <suzanverberne@ISCs-MacBook-Pro-3.local>
2017-07-12 14:08:21 +0200

    added functionality to use as package

--- pairwise.py
+++ pairwise.py
@@ -21,8 +21,6 @@ import csv
 import numpy as np
 from sklearn.linear_model import SGDClassifier
 
-trainfile = sys.argv[1]
-testfile = sys.argv[2]
 
 def get_vectors_per_groupid (filename):
     vectors_per_groupid = dict()
@@ -59,20 +57,23 @@ def pairwise_transform_trainset (vectors_for_one_group):
     group_id = str(int(vectors_for_one_group[0][0]))
     #print (group_id)
     for vector in vectors_for_one_group:
-        if vector[-1] == 1:
+        if vector[-1] == 1 or vector[-1] == 'yes':
             positive_examples.append(vector[0:])
+            #print ("pos example:",vector)
         else:
             negative_examples.append(vector[0:])
+            #print ("neg example:",vector)
     pairwise_data = []
 
     for pos in positive_examples:
         for neg in negative_examples:
-            #print (pos)
+            #print (pos[1],neg[1])
             if pos[1] != neg[1]:
                 pair_id = str(int(pos[1]))+"-"+str(int(neg[1]))
                 #print (group_id,pair_id)
                 paired1 = [group_id,pair_id]
-                diff1 = pos[2:-2]-neg[2:-2]
+                #print (np.array(pos[2:-2],dtype='float'))
+                diff1 = np.array(pos[2:-2],dtype='float')-np.array(neg[2:-2],dtype='float')
                 # the last item (the label) should not be part of the vectors that are subtracted
 
                 for i in diff1:
@@ -82,7 +83,7 @@ def pairwise_transform_trainset (vectors_for_one_group):
 
                 pair_id = str(int(neg[1]))+"-"+str(int(pos[1]))
                 paired2 = [group_id,pair_id]
-                diff2 = neg[2:-2]-pos[2:-2]
+                diff2 = np.array(neg[2:-2],dtype='float')-np.array(pos[2:-2],dtype='float')
                 for i in diff2:
                     paired2.append(i)
                 paired2.append(0)
@@ -104,7 +105,7 @@ def pairwise_transform_testset (vectors_for_one_group):
             if vector1[1] != vector2[1]:
                 pair_id = str(int(vector1[1]))+"-"+str(int(vector2[1]))
                 paired1 = [group_id,pair_id]
-                diff1 = vector1[2:-2]-vector2[2:-2]
+                diff1 = np.array(vector1[2:-2],dtype='float')-np.array(vector2[2:-2],dtype='float')
                 # the last item (the label) should not be part of the vectors that are subtracted
                 for i in diff1:
                     paired1.append(i)
@@ -112,7 +113,7 @@ def pairwise_transform_testset (vectors_for_one_group):
 
                 pair_id = str(int(vector2[1]))+"-"+str(int(vector1[1]))
                 paired2 = [group_id,pair_id]
-                diff2 = vector2[2:-2]-vector1[2:-2]
+                diff2 = np.array(vector2[2:-2],dtype='float')-np.array(vector1[2:-2],dtype='float')
                 for i in diff2:
                     paired2.append(i)
                 paired2.append(-1) # -1 is unknown label
@@ -191,112 +192,116 @@ def compute_recall(model,reference):
     else:
         return 1
 
-
-print("Get data from feature files")
-vectors_per_groupid_trainset = get_vectors_per_groupid(trainfile)
-vectors_per_groupid_testset = get_vectors_per_groupid(testfile)
-
-
-print("Do the pairwise transform for the training set")
-pairwise_data_train = []
-for group_id in vectors_per_groupid_trainset:
-    vectors_for_this_groupid = vectors_per_groupid_trainset[group_id]
-    #print (vectors_for_this_groupid)
-    pairwise_data = pairwise_transform_trainset(np.array(vectors_for_this_groupid))
-    for vector in pairwise_data:
-        pairwise_data_train.append(vector)
-    #print (pairwise_data)
-
-print("Do the pairwise transform for the test set")
-selectedposts_human = set()
-pairwise_data_test = []
-for group_id in vectors_per_groupid_testset:
-    vectors_for_this_groupid = vectors_per_groupid_testset[group_id]
-    #print (vectors_for_this_groupid)
-    for vector in vectors_for_this_groupid:
-        if vector[-1] == 1:
-            #print (str(int(vector[1])))
-            selectedposts_human.add(str(int(vector[1])))
-    pairwise_data = pairwise_transform_testset(np.array(vectors_for_this_groupid))
-    for vector in pairwise_data:
-        pairwise_data_test.append(vector)
+if __name__ == "__main__":
+    trainfile = sys.argv[1]
+    testfile = sys.argv[2]
 
 
-print("Convert to numpy arrays")
+    print("Get data from feature files")
+    vectors_per_groupid_trainset = get_vectors_per_groupid(trainfile)
+    vectors_per_groupid_testset = get_vectors_per_groupid(testfile)
 
-x_train = np.array([i[2:-2] for i in pairwise_data_train])
-y_train = np.array([i[-1] for i in pairwise_data_train])
-print("Train X dimensions:",x_train.shape)
-print("Train y dimensions:",y_train.shape)
-x_test = np.array([i[2:-2] for i in pairwise_data_test])
-group_id_array_test = np.array([i[0] for i in pairwise_data_test])
-item_pair_id_array_test = np.array([i[1] for i in pairwise_data_test])
 
-print("Test X dimensions:",x_test.shape)
-#print ("Test items:",group_id_array_test,item_pair_id_array_test)
+    print("Do the pairwise transform for the training set")
+    pairwise_data_train = []
+    for group_id in vectors_per_groupid_trainset:
+        vectors_for_this_groupid = vectors_per_groupid_trainset[group_id]
+        #print (vectors_for_this_groupid)
+        pairwise_data = pairwise_transform_trainset(np.array(vectors_for_this_groupid))
+        for vector in pairwise_data:
+            pairwise_data_train.append(vector)
+        #print (pairwise_data)
 
-'''
-PAIRWISE PREFERENCE LEARNING
-'''
+    print("Do the pairwise transform for the test set")
+    selecteditems_human = set()
+    pairwise_data_test = []
+    for group_id in vectors_per_groupid_testset:
+        vectors_for_this_groupid = vectors_per_groupid_testset[group_id]
+        #print (vectors_for_this_groupid)
+        for vector in vectors_for_this_groupid:
+            if vector[-1] == 1:
+                #print (str(int(vector[1])))
+                selecteditems_human.add(str(int(vector[1])))
+        pairwise_data = pairwise_transform_testset(np.array(vectors_for_this_groupid))
+        for vector in pairwise_data:
+            pairwise_data_test.append(vector)
 
-print("Train classifier on pairwise data")
-#clf = SVC()
-#clf.fit(x_train,y_train)
-clf = SGDClassifier(loss="hinge", penalty="l2")
-clf.fit(x_train,y_train)
 
-print ("Make predictions on testset")
-predicted = clf.predict(x_test)
-#print(predicted)
+    print("Convert to numpy arrays")
 
-print ("Greedy sort parwise preferences")
-'''
-The binary classification on the pairwise test data gives a prediction from each pair of test items:
-which of the two should be ranked higher. From these pairwise preferences a ranking can be created
-using a greedy sort algorithm.
-'''
-pairwise_preferences = dict()
-set_of_items_in_testset_per_group_id = dict()
-k=0
-for pred in predicted:
-    group_id = group_id_array_test[k]
-    item_pair_id = item_pair_id_array_test[k]
+    x_train = np.array([i[2:-2] for i in pairwise_data_train])
+    y_train = np.array([i[-1] for i in pairwise_data_train])
+    print("Train X dimensions:",x_train.shape)
+    print("Train y dimensions:",y_train.shape)
+    x_test = np.array([i[2:-2] for i in pairwise_data_test])
+    group_id_array_test = np.array([i[0] for i in pairwise_data_test])
+    item_pair_id_array_test = np.array([i[1] for i in pairwise_data_test])
 
-    (item_id1,item_id2) = str(item_pair_id).split(sep="-")
-    #(item_id1,item_id2) = "-".split(item_pair_id)
-    #print (group_id,item_id1,item_id2,pred)
-    pairwise_preferences[(item_id1,item_id2)] = pred
-    set_of_items = set()
-    if group_id in set_of_items_in_testset_per_group_id:
+    print("Test X dimensions:",x_test.shape)
+    #print ("Test items:",group_id_array_test,item_pair_id_array_test)
+
+    '''
+    PAIRWISE PREFERENCE LEARNING
+    '''
+
+    print("Train classifier on pairwise data")
+    #clf = SVC()
+    #clf.fit(x_train,y_train)
+    clf = SGDClassifier(loss="hinge", penalty="l2")
+    clf.fit(x_train,y_train)
+
+    print ("Make predictions on testset")
+    predicted = clf.predict(x_test)
+    #print(predicted)
+
+    print ("Greedy sort parwise preferences")
+    '''
+    The binary classification on the pairwise test data gives a prediction from each pair of test items:
+    which of the two should be ranked higher. From these pairwise preferences a ranking can be created
+    using a greedy sort algorithm.
+    '''
+    pairwise_preferences = dict()
+    set_of_items_in_testset_per_group_id = dict()
+    k=0
+    for pred in predicted:
+        group_id = group_id_array_test[k]
+        item_pair_id = item_pair_id_array_test[k]
+
+        (item_id1,item_id2) = str(item_pair_id).split(sep="-")
+        #(item_id1,item_id2) = "-".split(item_pair_id)
+        #print (group_id,item_id1,item_id2,pred)
+        pairwise_preferences[(item_id1,item_id2)] = pred
+        set_of_items = set()
+        if group_id in set_of_items_in_testset_per_group_id:
+            set_of_items = set_of_items_in_testset_per_group_id[group_id]
+        if item_id1 not in set_of_items:
+            set_of_items.add(item_id1)
+        if item_id2 not in set_of_items:
+            set_of_items.add(item_id2)
+        set_of_items_in_testset_per_group_id[group_id] = set_of_items
+        k += 1
+
+    ranked_itemids_per_group = dict()
+    for group_id in set_of_items_in_testset_per_group_id:
         set_of_items = set_of_items_in_testset_per_group_id[group_id]
-    if item_id1 not in set_of_items:
-        set_of_items.add(item_id1)
-    if item_id2 not in set_of_items:
-        set_of_items.add(item_id2)
-    set_of_items_in_testset_per_group_id[group_id] = set_of_items
-    k += 1
+        sorted_items = greedy_sort(set_of_items,pairwise_preferences)
+        ranked_itemids_per_group[group_id] = sorted_items
+        #print(group_id,sorted_items)
 
-ranked_postids_per_thread = dict()
-for group_id in set_of_items_in_testset_per_group_id:
-    set_of_items = set_of_items_in_testset_per_group_id[group_id]
-    sorted_items = greedy_sort(set_of_items,pairwise_preferences)
-    ranked_postids_per_thread[group_id] = sorted_items
-    #print(group_id,sorted_items)
+    print("Evaluate: create table for Precision-Recall graph")
 
-print("Evaluate: create table for Precision-Recall graph")
-
-for cutoff in range (1,10):
-    selectedposts_model = set()
-    for threadid in ranked_postids_per_thread:
-        ranked_postids = ranked_postids_per_thread[threadid]
-        k=0
-        for postid in ranked_postids:
-            k +=1
-            if k <= cutoff:
-                selectedposts_model.add(postid)
-    recall = compute_recall(selectedposts_model,selectedposts_human)
-    precision = compute_precision(selectedposts_model,selectedposts_human)
-    f1 = 2*(precision*recall)/(precision+recall)
-    print ("pairwise_SGD","\t",cutoff,"\t",recall,"\t",precision,"\t",f1)
+    for cutoff in range (1,10):
+        selecteditems_model = set()
+        for groupid in ranked_itemids_per_group:
+            ranked_itemids = ranked_itemids_per_group[groupid]
+            k=0
+            for itemid in ranked_itemids:
+                k +=1
+                if k <= cutoff:
+                    selecteditems_model.add(itemid)
+        recall = compute_recall(selecteditems_model,selecteditems_human)
+        precision = compute_precision(selecteditems_model,selecteditems_human)
+        f1 = 2*(precision*recall)/(precision+recall)
+        print ("pairwise_SGD","\t",cutoff,"\t",recall,"\t",precision,"\t",f1)
 
 
